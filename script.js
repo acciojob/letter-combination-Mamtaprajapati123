2dbbf14dafa537abeeb6ef56ae522e109200fc7f
Mamtaprajapati123 <124058185+Mamtaprajapati123@users.noreply.github.com>
2023-05-12 14:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 function letterCombinations(input_digit) {
-  //Complete the function
-}
+  const digitToLetters = {
+    0: '0',
+    1: '1',
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
 
-module.exports = letterCombinations;
+  function recurse(digits) {
+    if (digits.length === 0) {
+      return [''];
+    }
+
+    const currentDigit = digits[0];
+    const remainingDigits = digits.slice(1);
+    const letters = digitToLetters[currentDigit];
+    const combinations = [];
+
+    for (const letter of letters) {
+      const suffixes = recurse(remainingDigits);
+
+      for (const suffix of suffixes) {
+        combinations.push(letter + suffix);
+      }
+    }
+
+    return combinations;
+  }
+
+  return recurse(input_digit).sort();
+}
